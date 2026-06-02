d7f7522264601b04f864a5bf3de106b6a0603d08
Areef Khan <128778611+Areeff10@users.noreply.github.com>
2026-06-02 15:42:53 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medi-Assist',
+    version= '1.0.0',
+    author= 'Areef Khan',
+    author_email= 'areefkhan102004@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
